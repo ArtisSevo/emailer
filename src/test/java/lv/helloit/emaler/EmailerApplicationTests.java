474e45a307573e42ec2c1427a88b307aad24e3fa
Artis Sevostjanovs <seja1@inbox.lv>
2020-01-27 19:46:52 +0200

    First commit

---
+++ src/test/java/lv/helloit/emaler/EmailerApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.helloit.emaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
